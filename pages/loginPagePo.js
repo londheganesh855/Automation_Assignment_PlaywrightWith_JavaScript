201dc4d6d5ee4fb7c4f63d235f9ccd928a061660
Ganesh Londhe <londheganesh855@gmail.com>
2024-05-29 09:27:04 +0530

    login check

---
+++ pages/loginPagePo.js
@@ -0,0 +1,18 @@
+
+class LoginPageClass {
+    constructor(page) {
+        this.page = page;
+        this.emailField = page.getByLabel('Email address');
+        this.passwordField = page.getByLabel('Password');
+        this.signInButton = page.getByText('Login');
+    }
+
+    async loginUser() {
+        await this.signInButton.click();
+        await this.emailField.fill(process.env.userID);
+        await this.passwordField.fill(process.env.password);
+        await this.signInButton.click();
+    }
+}
+module.exports = { LoginPageClass };
+
---
+++ tests/loginTest.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import {LoginPageClass} from '../pages/loginPagePo';
+
+test('Verify Login functionality', async ({ page }) => {
+    test.setTimeout(120000)
+    const loginPage = new LoginPageClass(page);
+    await page.goto("/");
+    await loginPage.loginUser();
+})
+
+
