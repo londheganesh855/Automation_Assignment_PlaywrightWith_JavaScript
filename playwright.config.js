e25b2eb544ca1eeacc5f35d71b46a2b7cfc3d62a
Ganesh Londhe <londheganesh855@gmail.com>
2024-05-30 01:28:57 +0530

    Updated yml file

--- playwright.config.js
+++ playwright.config.js
@@ -28,6 +28,15 @@ module.exports = defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: process.env.baseUrl,
 
+      // Take a screenshot on failure
+    screenshot: 'only-on-failure',
+
+    // Record video for each test
+    video: 'retain-on-failure',
+
+    // Enable headless mode
+    headless: true,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -71,10 +80,10 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
